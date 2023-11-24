92de54b1880f3bd7e45e29600642035944d908bb
Kevin Jung <kev.jung.ii@gmail.com>
2023-11-24 00:29:20 -0500

    adds card component

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -3,17 +3,20 @@ import './RecipeCard.css'
 interface Recipe {
 	title: string
 	image: string
+	category: string
+	time: string
 }
 
 function RecipeCard(props: { recipe: Recipe }) {
   return (
-    <>
+    <div className='card-container'>
 		<div className='card' >
 			<h3 className='recipe-title'>{props.recipe.title}</h3>
-			<img className='recipe-img' src='/sous-chef.jpeg' width='200' height='200' alt='recipe image' />
+			<p className='recipe-time'>{props.recipe.time}</p>
+			<img className='recipe-img' src='/sous-chef.jpeg' width='300' height='400' alt='recipe image' />
 		</div>
 
-    </>
+    </div>
   )
 }
 
